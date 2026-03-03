729bb5d2b497cc6a70835dbb37f4a079bf925351
dingmu2019 <517709151@qq.com>
2026-03-04 01:24:58 +0800

    chore: bump version to 0.34.0

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import versionJson from '../version.json'
 export const SYSTEM_CONFIG = {
   name: 'AdminSys',
   version: versionJson.version,
-  build: '2026.03.03-stable',
+  build: '2026.03.04-stable',
   apiBaseUrl: process.env.NEXT_PUBLIC_API_URL || '/api',
   copyright: '© 2026 AdminSys Inc.',
 };
